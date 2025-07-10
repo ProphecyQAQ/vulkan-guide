fc5559760b171c187a562242fa630edb895ecd8a
baolinchang <baolinchang@tencent.com>
2025-07-10 15:24:35 +0800

    add load shder func

--- src/vk_pipelines.h
+++ src/vk_pipelines.h
@@ -1,7 +1,11 @@
 ﻿#pragma once 
 #include <vk_types.h>
+#include <vk_pipelines.h>
+#include <fstream>
+#include <vk_initializers.h>
 
 namespace vkutil {
 
+    bool load_shader_module(const char* filePath, VkDevice device, VkShaderModule* outShaderModule);
 
 };
