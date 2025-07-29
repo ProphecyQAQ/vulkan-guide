a2466fcdac85fb426fcd7be6e874423b864c6a4c
Lin Chang <lilyforeverx@gmail.com>
2025-07-29 14:47:09 +0800

    add camera

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,1 +1,48 @@
 #include "camera.h"
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+void Camera::update()
+{
+    glm::mat4 cameraRotation = get_rotation_matrix();
+    position += glm::vec3(cameraRotation * glm::vec4(velocity * 0.5f, 0.f));
+}
+
+void Camera::process_SDL_event(SDL_Event& e)
+{
+    if (e.type == SDL_KEYDOWN) {
+        if (e.key.keysym.sym == SDLK_w) { velocity.z = -1; }
+        if (e.key.keysym.sym == SDLK_s) { velocity.z = 1; }
+        if (e.key.keysym.sym == SDLK_a) { velocity.x = -1; }
+        if (e.key.keysym.sym == SDLK_d) { velocity.x = 1; }
+    }
+
+    if (e.type == SDL_KEYUP) {
+        if (e.key.keysym.sym == SDLK_w) { velocity.z = 0; }
+        if (e.key.keysym.sym == SDLK_s) { velocity.z = 0; }
+        if (e.key.keysym.sym == SDLK_a) { velocity.x = 0; }
+        if (e.key.keysym.sym == SDLK_d) { velocity.x = 0; }
+    }
+
+    if (e.type == SDL_MOUSEMOTION)
+    {
+        yaw += (float)e.motion.xrel / 200.f;
+        pitch -= (float)e.motion.yrel / 200.f;
+    }
+}
+
+glm::mat4 Camera::get_view_matrix()
+{
+    glm::mat4 translation = glm::translate(position);
+    glm::mat4 rotation = get_rotation_matrix();
+
+    return glm::inverse(translation * rotation);
+}
+
+glm::mat4 Camera::get_rotation_matrix()
+{
+    glm::quat pitchRotation = glm::angleAxis(pitch, glm::vec3 { 1.f, 0.f, 0.f });
+    glm::quat yawRotation = glm::angleAxis(yaw, glm::vec3 { 0.f, -1.f, 0.f });
+
+    return glm::toMat4(yawRotation) * glm::toMat4(pitchRotation);
+}
--- src/camera.h
+++ src/camera.h
@@ -1,7 +1,21 @@
 
 #include <vk_types.h>
+#include <SDL_events.h>
 
 class Camera {
 public:
+    glm::vec3 velocity;
+    glm::vec3 position;
 
+    // vertical rotation
+    float pitch { 0.f };
+    // horizontal rotation
+    float yaw { 0.f };
+
+    glm::mat4 get_view_matrix();
+    glm::mat4 get_rotation_matrix();
+
+    void process_SDL_event(SDL_Event& event);
+
+    void update();
 };
