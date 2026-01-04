21f40ee3ab6415c05b63fc1917ecdb5856d5a264
cl <changblqqq@outlook.com>
2026-01-04 23:49:33 +0800

    init vulkan cmd buffer

---
+++ src/RHI/Vulkan/VulkanCommandBuffer.cpp
@@ -0,0 +1,49 @@
+#include <Vulkan/VulkanCommandBuffer.h>
+
+// ------------------------------ VulkanCommandBuffer -----------------------
+VulkanCommandBuffer::VulkanCommandBuffer(VulkanDevice& device, VulkanCommandBufferPool& cmdPool)
+    : device(device), cmdPool(cmdPool)
+{
+    VkCommandBufferAllocateInfo allocInfo = {};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = cmdPool.getHandle();
+    allocInfo.level = (getType() == VK_CMD_BUFFER_TYPE_PRIMARY) ? VK_COMMAND_BUFFER_LEVEL_PRIMARY : VK_COMMAND_BUFFER_LEVEL_SECONDARY;
+    allocInfo.commandBufferCount = 1;
+
+    VK_CHECK(vkAllocateCommandBuffers(device.getDevice(), &allocInfo, &commandBuffer));
+}
+
+VulkanCommandBuffer::~VulkanCommandBuffer()
+{
+    vkFreeCommandBuffers(device.getDevice(), cmdPool.getHandle(), 1, &commandBuffer);
+}
+// ------------------------------ VulkanCommandBuffer -----------------------
+
+// ------------------------------ VulkanCommandBufferPool -----------------------
+VulkanCommandBufferPool::VulkanCommandBufferPool(VulkanDevice& device, uint32_t queueFamilyIndex, VulkanCommandBufferType type)
+    : device(device), queueFamilyIndex(queueFamilyIndex), type(type)
+{
+    VkCommandPoolCreateInfo info = {};
+    info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    info.pNext = nullptr;
+    info.queueFamilyIndex = queueFamilyIndex;
+
+    VK_CHECK(vkCreateCommandPool(device.getDevice(), &info, nullptr, &cmdPool));
+}
+
+VulkanCommandBufferPool::~VulkanCommandBufferPool()
+{
+    for (VulkanCommandBuffer* cmdBuffer : cmdBuffers)
+    {
+        delete cmdBuffer;
+    }
+    vkDestroyCommandPool(device.getDevice(), cmdPool, nullptr); 
+}
+
+VulkanCommandBuffer* VulkanCommandBufferPool::create()
+{
+    VulkanCommandBuffer* cmdBuffer = new VulkanCommandBuffer(device, *this);
+    cmdBuffers.push_back(cmdBuffer);
+    return cmdBuffer;
+}
+// ------------------------------ VulkanCommandBufferPool -----------------------
---
+++ src/RHI/Vulkan/VulkanCommandBuffer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Vulkan/VulkanDevice.h>
+#include <Vulkan/VulkanPCH.h>
+
+class VulkanCommandBuffer;
+
+enum VulkanCommandBufferType
+{
+    VK_CMD_BUFFER_TYPE_PRIMARY,
+    VK_CMD_BUFFER_TYPE_SECONDARY
+};
+
+class VulkanCommandBufferPool
+{
+public:
+    VulkanCommandBufferPool(VulkanDevice& device, uint32_t queueFamilyIndex, VulkanCommandBufferType type);
+    ~VulkanCommandBufferPool();
+
+    VulkanCommandBuffer* create();
+    VulkanCommandBufferType getType() const { return type; }
+    VkCommandPool getHandle() const { return cmdPool; }
+private:
+    std::vector<VulkanCommandBuffer*> cmdBuffers;
+    VkCommandPool cmdPool;
+
+    VulkanDevice& device;
+    uint32_t queueFamilyIndex;
+    VulkanCommandBufferType type;
+};
+
+class VulkanCommandBuffer
+{
+public:
+    VulkanCommandBuffer(VulkanDevice& device, VulkanCommandBufferPool& cmdPool);
+    ~VulkanCommandBuffer();
+
+    VkCommandBuffer getHandle() const { return commandBuffer; }
+    VulkanCommandBufferType getType() const { return cmdPool.getType(); }
+private:
+    VulkanDevice& device;
+    VulkanCommandBufferPool& cmdPool;
+    VkCommandBuffer commandBuffer;
+};
